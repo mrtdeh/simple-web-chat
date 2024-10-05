4e87bad58e980e5d4d794357ba22920092bcebc6
mrtdeh <mrtdeh@gmail.com>
2024-10-05 07:50:22 +0330

    fix: clean code

--- pkg/server/getMessage.go
+++ pkg/server/getMessage.go
@@ -1,39 +1,34 @@
 package server
 
 import (
-	database "api-channel/pkg/db"
-	"api-channel/pkg/models"
 	"api-channel/proto"
 	"context"
-	"fmt"
 )
 
-func (s *Server) GetMessages(ctx context.Context, req *proto.GetMessagesRequest) (*proto.Messages, error) {
+func (s *Server) GetMessages(ctx context.Context, req *proto.GetMessagesRequest) (*proto.MessagesResponse, error) {
 
-	db := database.GetInstance()
+	// db := database.GetInstance()
 
-	var messages []models.Message
-	res := db.Model(&models.Message{ChatID: 1}).Preload("Sender").Preload("Replies").Find(&messages)
-	if res.Error != nil {
-		return nil, res.Error
-	}
+	// var messages []models.Message
+	// res := db.Model(&models.Message{ChatID: 1}).Preload("Sender").Preload("Replies").Find(&messages)
+	// if res.Error != nil {
+	// 	return nil, res.Error
+	// }
 
-	var data []*proto.MessageData
-	for i, m := range messages {
-		data = append(data, &proto.MessageData{
-			SenderId:  uint32(m.Sender.ID),
-			MessageId: uint32(m.ID),
-			Content:   m.Content,
-		})
-		fmt.Println("msg id : ", m.ID)
-		for _, r := range m.Replies {
-			fmt.Println("reply to :", r.ReplyMessageId)
-			data[i].Replies = append(data[i].Replies, &proto.ReplayTo{
-				Id: uint32(r.ReplyMessageId),
-				// Type: r.Type,
-			})
-		}
-	}
+	// var data []*proto.MessageData
+	// for i, m := range messages {
+	// 	data = append(data, &proto.MessageData{
+	// 		SenderId:  uint32(m.Sender.ID),
+	// 		MessageId: uint32(m.ID),
+	// 		Content:   m.Content,
+	// 	})
+	// 	for _, r := range m.Replies {
+	// 		data[i].RepliedMessages = append(data[i].RepliedMessages, &proto.RepliedMessage{
+	// 			MessageId: uint32(r.ReplyMessageId),
+	// 		})
+	// 	}
+	// }
 
-	return &proto.Messages{Data: data}, nil
+	// return &proto.Messages{Data: data}, nil
+	return nil, nil
 }
