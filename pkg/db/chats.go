2935384370d5a2dab6d2c54faccc8ef8eea83d9c
mrtdeh <mrtdeh@gmail.com>
2024-10-22 16:44:47 +0330

    fix: update models

--- pkg/db/chats.go
+++ pkg/db/chats.go
@@ -1,32 +1,103 @@
 package database
 
 import (
-	"api-channel/pkg/models"
 	"api-channel/proto"
 )
 
+type Chat struct {
+	ChatID             uint
+	ChatName           string
+	Type               string
+	Mute               bool
+	AvatarUrl          string
+	LastMessage        string
+	LastMessageRead    string
+	UnreadMessageCount int
+}
+
 func GetChats(userId uint) (*proto.ChatsResponse, error) {
 
-	var user models.User
-	res := db.First(&user, userId).
-		Preload("Chats").
-		Preload("Replies").
-		Preload("Replies.ReplyMessage").
-		Preload("Replies.Thumbnails").
-		Preload("Replies.Thumbnails.Thumbnail").
-		Preload("Attachments").
-		Preload("Attachments.Thumbnails", "type = ?", "placeholder").
-		Find(&chats)
+	// var user models.User
+	// res := db.First(&user, userId).Preload("Chats").Preload("Group")
 
-	if res.Error != nil {
-		return nil, res.Error
+	// if res.Error != nil {
+	// 	return nil, res.Error
+	// }
+
+	// var data []Chat
+	// for _, chat := range user.Chats {
+	// 	c := Chat{ChatID: chat.ID}
+	// 	if chat.IsGroup {
+	// 		c.ChatName = chat.Group.Name
+	// 		c.AvatarUrl = chat.Group.AvatarUrl
+
+	// 		res :=  db.Table("chats").
+	// 	InnerJoins("JOIN chat_members cm ON chats.id = cm.chat_id AND cm.user_id = ?",userId)
+	// 		c.Mute = chat.
+	// 	} else {
+
+	// 	}
+	// }
+
+	var chats []struct {
+		ChatID             uint
+		ChatName           string
+		Type               string
+		Mute               bool
+		AvatarUrl          string
+		LastMessage        string
+		LastMessageRead    string
+		UnreadMessageCount int
 	}
 
-	var data []*proto.MessagesResponse_MessageData
-	for _, m := range messages {
+	db.Table("chats").
+		// فیلتر کردن چت‌ها برای کاربر خاص
+		InnerJoins("JOIN chat_members cm1 ON chats.id = cm1.chat_id AND cm1.user_id = ?", userId).
+		// پیدا کردن طرف دیگر چت خصوصی
+		Joins("LEFT JOIN private_chats pc ON chats.id = pc.chat_id").
+		Joins("LEFT JOIN users u1 ON pc.user1_id = u1.id").
+		Joins("LEFT JOIN users u2 ON pc.user2_id = u2.id").
+		// اتصال به جدول گروه‌ها برای چت‌های گروهی
+		Joins("LEFT JOIN groups g ON chats.id = g.chat_id").
+		// Connect to Last_Message_Reads table
+		// Joins("LEFT JOIN last_message_reads lmr ON lmr.chat_id = chats.id AND lmr.user_id = ?", userId).
+		// // محاسبه تعداد پیام‌های خوانده‌نشده
+		// Joins("LEFT JOIN LATERAL (SELECT COUNT(*) FROM messages m WHERE m.chat_id = chats.id AND m.created_at > COALESCE(last_read_msg.created_at, '1970-01-01')) AS unread_msg_count ON chats.id = unread_msg_count.chat_id").
 
-	}
+		Joins("LEFT JOIN LATERAL (SELECT m.content, m.created_at FROM messages m WHERE m.chat_id = chats.id ORDER BY m.created_at DESC LIMIT 1) AS last_msg ON chats.id = last_msg.chat_id").
+		// پیوستن به آخرین پیامی که کاربر خوانده است
+		Joins("LEFT JOIN last_message_reads lmr ON lmr.chat_id = chats.id AND lmr.user_id = ?", userId).
+		// پیوستن به پیام خوانده‌شده توسط کاربر
+		Joins("LEFT JOIN messages last_read_msg ON last_read_msg.id = lmr.last_readed_message_id").
+		// محاسبه تعداد پیام‌های خوانده‌نشده
+		Joins("LEFT JOIN LATERAL (SELECT COUNT(*) FROM messages m WHERE m.chat_id = chats.id AND m.created_at > COALESCE(last_read_msg.created_at, '1970-01-01')) AS unread_msg_count ON chats.id = unread_msg_count.chat_id").
 
-	return &proto.MessagesResponse{Data: data}, nil
+		// فیلتر کردن چت‌ها برای کاربر خاص
+		// Where("cm1.user_id = ?", userId).
+		Select(`
+		chats.id AS chat_id,
+		CASE
+			WHEN chats.is_group THEN g.name            
+			WHEN pc.user1_id = ? THEN u2.username      
+			WHEN pc.user2_id = ? THEN u1.username      
+		END AS chat_name,
+		CASE 
+			WHEN chats.is_group THEN 'group'          
+			ELSE 'private'                            
+		END AS type,
+		cm1.mute AS mute,
+		CASE 
+			WHEN chats.is_group THEN g.avatar          
+			WHEN pc.user1_id = ? THEN u2.profile_picture 
+			WHEN pc.user2_id = ? THEN u1.profile_picture 
+		END AS avatar_url,
+		last_msg.content AS last_message,              
+		last_read_msg.content AS last_message_read,   
+		unread_msg_count.count AS unread_message_count 
+
+	`, userId, userId).
+		Scan(&chats)
+
+	return &proto.ChatsResponse{}, nil
 
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,6 +6,49 @@ import (
 	"gorm.io/gorm"
 )
 
+// type User struct {
+// 	gorm.Model
+// 	Username       string `gorm:"unique;not null"`
+// 	Password       string `gorm:"not null"`
+// 	DisplayName    string
+// 	PhoneNumber    string `gorm:"unique"`
+// 	Email          string `gorm:"unique"`
+// 	ProfilePicture string
+// 	Bio            string
+// 	CreatedAt      time.Time
+// 	LastLogin      time.Time
+// 	Chats          []Chat `gorm:"many2many:chat_members;"`
+// }
+
+// type Chat struct {
+// 	gorm.Model
+// 	Name     string
+// 	IsGroup  bool   `gorm:"default:false"`
+// 	Members  []User `gorm:"many2many:chat_members;"`
+// 	Messages []Message
+// 	Group    *Group `gorm:"foreignKey:ChatID"`
+// }
+
+// type Group struct {
+// 	gorm.Model
+// 	Name        string `gorm:"not null"`
+// 	Description string
+// 	AvatarUrl   string
+// 	OwnerID     uint      `gorm:"not null"`
+// 	ChatID      uint      `gorm:"not null"`
+// 	Chat        Chat      `gorm:"foreignKey:ChatID"`
+// 	Messages    []Message `gorm:"foreignKey:ChatID"`
+// 	Members     []User    `gorm:"many2many:chat_members;foreignKey:ChatID;joinForeignKey:ChatID;"`
+// }
+
+// type ChatMember struct {
+// 	ChatID   uint   `gorm:"primaryKey"`
+// 	UserID   uint   `gorm:"primaryKey"`
+// 	Role     string `gorm:"default:'member'"`
+// 	Mute     bool   `gorm:"default:false"`
+// 	JoinedAt time.Time
+// }
+
 type User struct {
 	gorm.Model
 	Username       string `gorm:"unique;not null"`
@@ -15,23 +58,43 @@ type User struct {
 	Email          string `gorm:"unique"`
 	ProfilePicture string
 	Bio            string
-	CreatedAt      time.Time
-	LastLogin      time.Time
 	Chats          []Chat `gorm:"many2many:chat_members;"`
 }
 
 type Chat struct {
 	gorm.Model
-	Name     string
-	IsGroup  bool   `gorm:"default:false"`
-	Members  []User `gorm:"many2many:chat_members;"`
-	Messages []Message
+	Name        string
+	IsGroup     bool   `gorm:"default:false"`
+	Members     []User `gorm:"many2many:chat_members;"`
+	Messages    []Message
+	Group       *Group       `gorm:"foreignKey:ChatID"` // رابطه با جدول گروه
+	PrivateChat *PrivateChat `gorm:"foreignKey:ChatID"` // رابطه برای چت خصوصی
+}
+
+type Group struct {
+	gorm.Model
+	Name        string `gorm:"not null"`
+	Description string
+	OwnerID     uint   `gorm:"not null"`
+	ChatID      uint   `gorm:"not null"`
+	Chat        Chat   `gorm:"foreignKey:ChatID"`
+	Members     []User `gorm:"many2many:chat_members;"`
+}
+
+type PrivateChat struct {
+	gorm.Model
+	ChatID      uint `gorm:"not null"`
+	LeftUserID  uint `gorm:"not null"` // کاربر اول
+	RightUserID uint `gorm:"not null"` // کاربر دوم
+	LeftUser    User `gorm:"foreignKey:LeftUserID"`
+	RightUser   User `gorm:"foreignKey:RightUserID"`
 }
 
 type ChatMember struct {
 	ChatID   uint   `gorm:"primaryKey"`
 	UserID   uint   `gorm:"primaryKey"`
 	Role     string `gorm:"default:'member'"`
+	Mute     bool   `gorm:"default:false"`
 	JoinedAt time.Time
 }
 
@@ -49,9 +112,10 @@ type Message struct {
 
 type LastMessageRead struct {
 	gorm.Model
-	UserID    uint `gorm:"not null"`
-	ChatID    uint `gorm:"not null"`
-	MessageID uint `gorm:"not null"`
+	UserID              uint `gorm:"not null"`
+	ChatID              uint `gorm:"not null"`
+	LastMessageID       uint `gorm:"not null"`
+	LastReadedMessageID uint `gorm:"not null"`
 }
 
 type Sticker struct {
@@ -90,17 +154,6 @@ type Thumbnail struct {
 	AttachmentID uint   `gorm:"not null"`
 	Base64       string // base64 of image file
 	Type         string // mini|small|placeholder
-}
-
-type Group struct {
-	gorm.Model
-	Name        string `gorm:"not null"`
-	Description string
-	OwnerID     uint      `gorm:"not null"`
-	ChatID      uint      `gorm:"not null"`
-	Chat        Chat      `gorm:"foreignKey:ChatID"`
-	Messages    []Message `gorm:"foreignKey:ChatID"`
-	Members     []User    `gorm:"many2many:chat_members;foreignKey:ChatID;joinForeignKey:ChatID;"`
 }
 
 // type GroupMember struct {
